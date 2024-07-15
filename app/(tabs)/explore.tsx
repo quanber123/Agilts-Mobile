4bbc1d6e15a06d799e2d047c923be075231bd834
quanber123 <quantm103@gmail.com>
2024-07-15 14:19:26 +0700

    first commit

---
+++ app/(tabs)/explore.tsx
@@ -0,0 +1,9 @@
+import { Text, View } from 'react-native';
+
+export default function TabTwoScreen() {
+  return (
+    <View className='flex-1 justify-center items-center'>
+      <Text className='text-3xl font-bold text-red-500'>Hello world</Text>
+    </View>
+  );
+}
---
+++ app/sign-in.tsx
@@ -0,0 +1,5 @@
+import { View } from 'react-native';
+
+export default function SignInScreen() {
+  return <View className='flex-1 justify-center items-center'>sign-in</View>;
+}
